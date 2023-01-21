e78017e67ac0488259fcadaeffb4842688beb2ea
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-01-21 20:31:59 +0900

    5. 쇠막대기

--- app/src/main/java/com/example/algorithmpractice/stackqueue/PostfixOperation.kt
+++ app/src/main/java/com/example/algorithmpractice/stackqueue/PostfixOperation.kt
@@ -3,7 +3,7 @@ package com.example.algorithmpractice.stackqueue
 import java.util.*
 
 /**4. 후위식 연산(postfix)
-@since 2023.1.18.Fri
+@since 2023.1.20.Fri
 설명
 후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
 만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 12입니다.
