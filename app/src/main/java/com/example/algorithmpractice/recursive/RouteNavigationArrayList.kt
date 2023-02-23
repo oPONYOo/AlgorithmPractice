0ce4f80fdd4a05ca61ba3d8ab5fbcd6f7fcb40c1
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-02-23 23:35:43 +0900

    12. 경로 탐색(인접리스트)

--- app/src/main/java/com/example/algorithmpractice/recursive/RouteNavigationArrayList.kt
+++ app/src/main/java/com/example/algorithmpractice/recursive/RouteNavigationArrayList.kt
@@ -1,9 +1,10 @@
 package com.example.algorithmpractice.recursive
 
 import java.util.*
+import kotlin.collections.ArrayList
 
-/**11. 경로 탐색(인접행렬)
-@since 2023.2.22.Wed
+/**12. 경로 탐색(인접리스트)
+@since 2023.2.23.Thu
 
 설명
 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를
@@ -48,6 +49,7 @@ import java.util.*
 [풀기 전]
 중복된 노드가 없어야한다.
 방문한 노드는 또 방문할 수 없음
+인접리스트는 정점의 개수 너무 많을 때 사용한다.
 
 
 [풀고 난 후]
@@ -58,20 +60,20 @@ import java.util.*
 private var n = 0
 private var m = 0
 private var answer = 0
-private var graph = Array(n) { Array(m) { 0 } }
+private var graph = ArrayList<ArrayList<Int>>()
 private var ch = IntArray(0)
 
 private fun DFS(v: Int) { // v: 현재 노드
     if (v == n) answer++ // 정점이면 ++
-    else { // 아니면 1부터 n+1까지 반복문 실행
-        for (i in 1 until n + 1) {
-            if (graph[v][i] == 1 && ch[i] == 0) { // v -> i로 갈 수 있는지, 방문한적 없는지 체크
-                ch[i] = 1 // 방문했다고 체크
-                DFS(i)
-                // back
-                ch[i] = 0 // 돌아오면 방문한거 취소
+    else {
+        graph[v].forEach {nv -> // graph[v]: v번 ArrayList
+            if (ch[nv] == 0) {
+                ch[nv] = 1
+                DFS(nv)
+                ch[nv] = 0
             }
         }
+
     }
 }
 
@@ -79,12 +81,14 @@ fun main() {
     val sc = Scanner(System.`in`)
     n = sc.nextInt() // 노드 개수
     m = sc.nextInt() // 간선
-    graph = Array(n + 1) { Array(n + 1) { 0 } } // 인접행렬
+    for (i in 0 until n+1) {
+        graph.add(ArrayList())
+    }
     ch = IntArray(n + 1)
     for (i in 0 until m) {
         val a = sc.nextInt()
         val b = sc.nextInt()
-        graph[a][b] = 1
+        graph[a].add(b) // a번 ArrayList에 갈 수 있는 노드 add
     }
     ch[1] = 1 // 방문했었는지 체크
     DFS(1)
